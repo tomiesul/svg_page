4f89cb1ba4bcfffade5007eacc66dcc7cfe40d56
tomiesul <99337818+tomiesul@users.noreply.github.com>
2024-02-26 00:32:24 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let light = document.getElementById("Layer 3")
+
+light.addEventListener("click", () => {
+    document.getElementById("textbox").innerText="You have clicked on the light beam of the apple!"
+})
